900312617f9ade4d188e09b7bc9f6e059444ab7b
Norouzi <Norouzi@N-Norouzi>
2024-11-26 23:17:19 +0330

    Implemented CSRService to handle business logic for CSR operations

---
+++ src/main/java/com/example/CBS/service/CSRService.java
@@ -0,0 +1,82 @@
+package com.example.CBS.service;
+
+import org.springframework.stereotype.Service;
+import com.example.CBS.repository.*;
+import com.example.CBS.exception.CSRAlreadyExistsException;
+import com.example.CBS.exception.CSRNotFoundException;
+import com.example.CBS.model.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class CSRService implements CrudService<CSR,Integer>{
+	
+	private CSRRepository repositoryCSR;
+	@Autowired
+	private CSRAuthorityRepository repositoryCSRAuthority;
+	
+	@Autowired
+	public CSRService(CSRRepository repositoryCSR) {
+		this.repositoryCSR=repositoryCSR;
+	}
+
+	
+    @Override
+    public List<CSR> findAll() {
+    	try {
+    		return repositoryCSR.findAll();
+    	}
+    	catch(Exception e) {
+    		throw new RuntimeException("An unexpected error occurred while retrieving the CSR. Please try again later.");
+    	}
+        
+    }
+
+    @Override
+    public CSR findById(Integer id) {
+    	try {
+        	return repositoryCSR.findById(id)
+                    .orElseThrow(() -> new CSRNotFoundException("Did not find CSR id - " + id));
+    	}catch(CSRNotFoundException e) {
+    		throw e;
+    	}catch(Exception e) {
+    		throw new RuntimeException("An unexpected error occurred.");
+    	}
+
+    }
+
+    @Override
+    public CSR save(CSR theCSR) {
+    	try {
+            Optional<CSR> existingCSR = repositoryCSR.findBycsrName(theCSR.getCsrName());
+            existingCSR.ifPresent(csr ->{
+            	throw new CSRAlreadyExistsException("CSR with name " + theCSR.getCsrName() + " already exists.");
+            });
+        	return repositoryCSR.save(theCSR);
+    	}catch(CSRAlreadyExistsException e) {
+    		throw e;
+    	}catch(Exception e) {
+    		throw new RuntimeException("An unexpected error occurred.");
+    	}
+
+    }
+
+    @Override
+    public void deleteById(Integer theId) {
+        try {
+            CSR csr = repositoryCSR.findById(theId)
+                    .orElseThrow(() -> new CSRNotFoundException("Did not find CSR with ID - " + theId));
+
+            // Delete the CSR if found
+            repositoryCSR.deleteById(theId);
+        }catch(Exception e) {
+        	throw new RuntimeException("An unexpected error occurred.");
+        }
+
+    	
+    }
+    	
+}
