2bd56145b2cf55a63454949135f7e7b61fa2e46f
Norouzi <Norouzi@N-Norouzi>
2024-11-26 23:25:15 +0330

    Add global exception handling with custom error response and invalid input exception

---
+++ src/main/java/com/example/CBS/exception/GlobalExceptionHandler.java
@@ -0,0 +1,66 @@
+package com.example.CBS.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(CSRNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleCSRNotFoundException(CSRNotFoundException ex, WebRequest request){
+		
+		ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+		return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(CSRAlreadyExistsException.class)
+	public ResponseEntity<ErrorResponse> handleCSRAlreadyExistsException(CSRAlreadyExistsException ex,WebRequest request){
+		
+		ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+		return new ResponseEntity<>(errorResponse,HttpStatus.CONFLICT);
+	}
+		
+	
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex,WebRequest request) {
+        
+		ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+		return new ResponseEntity<>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+		
+    }
+    
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(InvalidInputException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/example/CBS/exception/InvalidInputException.java
@@ -0,0 +1,10 @@
+package com.example.CBS.exception;
+
+public class InvalidInputException extends RuntimeException{
+	public InvalidInputException(String message) {
+		super(message);
+	}
+	public InvalidInputException(String message,Throwable cause) {
+		super(message,cause);
+	}
+}
