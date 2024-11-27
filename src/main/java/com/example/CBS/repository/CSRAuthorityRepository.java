edc834bd46eda9ea15efa4b4aae42bd24054d968
Norouzi <Norouzi@N-Norouzi>
2024-11-27 21:19:35 +0330

    data access layer and inner join to fetch CSR authorities

--- src/main/java/com/example/CBS/repository/CSRAuthorityRepository.java
+++ src/main/java/com/example/CBS/repository/CSRAuthorityRepository.java
@@ -3,10 +3,21 @@ package com.example.CBS.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.CBS.model.CSRAuthority;
 @Repository
 public interface CSRAuthorityRepository extends JpaRepository<CSRAuthority, Integer> {
-	List<CSRAuthority> findByCsr_csrId(Integer csrId);
+	//List<CSRAuthority> findByCsr_csrId(Integer csrId);
+	
+
+    
+	@Query(value ="SELECT authority.role " +
+            "FROM CSR csr " +
+            "JOIN csr_authority_mapping mapping ON csr.csr_id = mapping.csr_id " +
+            "JOIN CSR_authority authority ON mapping.authority_id = authority.authority_id " +
+            "WHERE csr.csr_id = :csrId", nativeQuery = true)
+	List<String> findCSRByCSRAuthorityField(@Param("csrId") Integer csrId);
 }
