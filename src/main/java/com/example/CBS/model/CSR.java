394e69a0a974455187d40abf880c7e00b8a2a7dd
Norouzi <Norouzi@N-Norouzi>
2024-11-27 21:13:37 +0330

    Modify CSR entity to support business authority addition

--- src/main/java/com/example/CBS/model/CSR.java
+++ src/main/java/com/example/CBS/model/CSR.java
@@ -1,15 +1,15 @@
 package com.example.CBS.model;
-import java.util.List;
 
+import java.util.Set;
 import jakarta.persistence.*;
 
 @Entity
 public class CSR {
-	
+
     @Id
     @Column(name = "csr_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer  csrId;
+    private Integer csrId;
 
     @Column(name = "name_CSR")
     private String csrName;
@@ -23,57 +23,64 @@ public class CSR {
     @Column(name = "available")
     private Boolean available;
 
-    @OneToMany(mappedBy = "csr", cascade = CascadeType.ALL, orphanRemoval = true,fetch = FetchType.EAGER)
-    private List<CSRAuthority> csrAuthorities;
+    @ManyToMany
+    @JoinTable(
+        name = "csr_authority_mapping", // Join table name
+        joinColumns = @JoinColumn(name = "csr_id"), // Foreign key for CSR
+        inverseJoinColumns = @JoinColumn(name = "authority_id") // Foreign key for CSRAuthority
+    )
+    private Set<CSRAuthority> csrAuthorities;
 
-	public Integer getCsrId() {
-		return csrId;
-	}
+    public Integer getCsrId() {
+        return csrId;
+    }
 
-	public void setCsrId(Integer  csrId) {
-		this.csrId = csrId;
-	}
+    public void setCsrId(Integer csrId) {
+        this.csrId = csrId;
+    }
 
-	public String getCsrName() {
-		return csrName;
-	}
+    public String getCsrName() {
+        return csrName;
+    }
 
-	public void setCsrName(String csrName) {
-		this.csrName = csrName;
-	}
+    public void setCsrName(String csrName) {
+        this.csrName = csrName;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-	public Boolean isAvailable() {
-		return available;
-	}
+    public Boolean isAvailable() {
+        return available;
+    }
 
-	public void setAvailable(Boolean available) {
-		this.available = available;
-	}
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
 
-	public List<CSRAuthority> getCsrAuthorities() {
-		return csrAuthorities;
-	}
+    public Set<CSRAuthority> getCsrAuthorities() {
+        return csrAuthorities;
+    }
 
-	public void setCsrAuthorities(List<CSRAuthority> csrAuthorities) {
-		this.csrAuthorities = csrAuthorities;
-	}
-	
-	
-	
+    public void setCsrAuthorities(Set<CSRAuthority> csrAuthorities) {
+        this.csrAuthorities = csrAuthorities;
+    }
+    
+    public void addAuthority(CSRAuthority authority) {
+        this.csrAuthorities.add(authority);
+        authority.getCsrs().add(this);
+    }
 }
