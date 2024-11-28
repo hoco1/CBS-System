1ae59d3888b3a8e629e568743ade836b77930411
Norouzi <Norouzi@N-Norouzi>
2024-11-28 13:28:43 +0330

    offer repository

---
+++ src/main/java/com/example/CBS/repository/OfferRepository.java
@@ -0,0 +1,9 @@
+package com.example.CBS.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.CBS.model.Offer;
+
+public interface OfferRepository extends JpaRepository<Offer,Integer>{
+
+}
