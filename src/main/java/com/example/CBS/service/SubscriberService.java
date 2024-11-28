3d9263e933c9a8c8cd42caf967a0e53b5e9233e6
Norouzi <Norouzi@N-Norouzi>
2024-11-28 22:38:56 +0330

    add find by id

--- src/main/java/com/example/CBS/service/SubscriberService.java
+++ src/main/java/com/example/CBS/service/SubscriberService.java
@@ -1,10 +1,14 @@
 package com.example.CBS.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.CBS.model.Subscriber;
 import com.example.CBS.repository.SubscriberRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class SubscriberService {
@@ -17,5 +21,10 @@ public class SubscriberService {
 	
 	public Subscriber RegisterSubscriber(Subscriber subscriber) {
 		return subscriberRepository.save(subscriber);
+	}
+	
+	public Subscriber findById(Integer subId) {
+		return subscriberRepository.findById(subId)
+				.orElseThrow(() -> new EntityNotFoundException("Subscriber not found"));
 	}
 }
