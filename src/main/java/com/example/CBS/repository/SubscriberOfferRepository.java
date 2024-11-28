52c7f257f77c345ec2c57db7b0e0d76cde41d807
Norouzi <Norouzi@N-Norouzi>
2024-11-28 22:38:15 +0330

    add subscriber offer repository

---
+++ src/main/java/com/example/CBS/repository/SubscriberOfferRepository.java
@@ -0,0 +1,9 @@
+package com.example.CBS.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.CBS.model.SubscriberOffer;
+
+public interface SubscriberOfferRepository extends JpaRepository<SubscriberOffer,Integer> {
+
+}
