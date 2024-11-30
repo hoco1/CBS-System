979430e1db4fdd88e73423ad5854184797a20b14
Norouzi <Norouzi@N-Norouzi>
2024-11-30 06:29:03 +0330

    Add exception throw for offer exclusion

--- src/main/java/com/example/CBS/service/SubscriberOfferService.java
+++ src/main/java/com/example/CBS/service/SubscriberOfferService.java
@@ -3,6 +3,9 @@ package com.example.CBS.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.CBS.exception.OfferExclusionException;
+import com.example.CBS.model.Offer;
+import com.example.CBS.model.Subscriber;
 import com.example.CBS.model.SubscriberOffer;
 import com.example.CBS.repository.SubscriberOfferRepository;
 
@@ -15,7 +18,13 @@ public class SubscriberOfferService {
 		this.subscriberOfferRepository=subscriberOfferRepository;
 	}
 	
-	public SubscriberOffer buyOffer(SubscriberOffer subscriberOffer) {
+	public SubscriberOffer buyOffer(SubscriberOffer subscriberOffer,Offer offer , Subscriber subscriber) {
+		
+		SubscriberOffer subOffer = subscriberOfferRepository.findBySubscriberAndOffer(subscriber, offer);
+		
+		if(subOffer!=null) {
+			throw new OfferExclusionException("Subscriber has already purchased this offer.");
+		}
 		return subscriberOfferRepository.save(subscriberOffer);
 	}
 }
