77ab2ddab0526f6fb0cdb0db6209644ac0aa82f9
Norouzi <Norouzi@N-Norouzi>
2024-11-30 06:27:37 +0330

    Add JPA query to retrieve purchased offers by subscriber

--- src/main/java/com/example/CBS/repository/SubscriberOfferRepository.java
+++ src/main/java/com/example/CBS/repository/SubscriberOfferRepository.java
@@ -1,9 +1,17 @@
 package com.example.CBS.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.CBS.model.Offer;
+import com.example.CBS.model.Subscriber;
 import com.example.CBS.model.SubscriberOffer;
 
 public interface SubscriberOfferRepository extends JpaRepository<SubscriberOffer,Integer> {
-
+	@Query("SELECT e FROM SubscriberOffer e WHERE e.subscriber=:subscriber AND e.offer=:offer")
+	SubscriberOffer findBySubscriberAndOffer(@Param("subscriber") Subscriber subscriber,
+												   @Param("offer") Offer offer);
 }
